c30377ce968150f0ff90645a28d3b3e52d8875f8
byron446 <byronbettsrandom@gmail.com>
2024-08-25 16:17:17 +0930

    works for all tests except clock on trace3
    the reason i did this in a branch is because I changed a lot of function signatures and stuff which might be frowned upon. This way, main retains an un-modified version of the code that we can start over from if necessary.

--- memsim.c
+++ memsim.c
@@ -1,58 +1,155 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 
 typedef struct {
         int pageNo;
         int modified;
 } page;
-enum    repl { random, fifo, lru, clock};
-int     createMMU( int);
-int     checkInMemory( int ) ;
-int     allocateFrame( int ) ;
-page    selectVictim( int, enum repl) ;
+
+typedef struct {
+	page page;
+	int usage_data;
+} pte;
+enum    repl { rng, fifo, lru, clock};
+int     createMMU(int);
+int     checkInMemory(int, int, enum repl);
+int     allocateFrame(int, int, enum repl);
+int 	findMinUsage();
+int 	findClockVictim();
+page    selectVictim(int, int, enum repl);
 const   int pageoffset = 12;            /* Page size is fixed to 4 KB */
-int     numFrames ;
+int     numFrames;
+int clock_hand = 0;
+pte* page_table;
 
 /* Creates the page table structure to record memory allocation */
-int     createMMU (int frames)
+int createMMU (int frames)
 {
 
         // to do
-
+		page_table = malloc(sizeof(pte)*frames);
+		if (page_table == NULL) return -1;
+		// Set all usage data to -1  (flag for unallocated)
+		for (int i = 0; i < frames; i++) {
+			page_table[i].usage_data = -1;
+		}
         return 0;
 }
 
 /* Checks for residency: returns frame no or -1 if not found */
-int     checkInMemory( int page_number)
+int checkInMemory(int page_number, int event_number, enum repl replace)
 {
         int     result = -1;
 
-        // to do
-
-
-        return result ;
+        // Loop through page table to find the requested page number
+		for (int i = 0; i < numFrames; i++) {
+			if (page_table[i].usage_data == -1) continue;
+			if (page_table[i].page.pageNo == page_number) {
+				result = i;
+				switch (replace)
+				{
+				case lru:
+					page_table[i].usage_data = event_number;
+					break;
+				case clock:
+					page_table[i].usage_data = 1;
+				default:
+					break;
+				}
+				break;
+			}
+		}
+        return result;
 }
 
 /* allocate page to the next free frame and record where it put it */
-int     allocateFrame( int page_number)
+int allocateFrame(int page_number, int event_number, enum repl replace)
 {
         // to do
-        return;
+		int i = 0;
+		while (page_table[i].usage_data != -1) i++;
+		page_table[i].page.pageNo = page_number;
+		page_table[i].page.modified = 0;
+		switch (replace) {
+			case lru:
+			case fifo:
+				page_table[i].usage_data = event_number;
+				break;
+			case clock:
+			case rng:
+				page_table[i].usage_data = 1;
+				break;
+		}
+        return i;
+}
+
+// Returns the index of the PTE with the lowest usage data value
+int findMinUsage() 
+{
+	int min_usage = page_table[0].usage_data;
+	int min_usage_index = 0;
+	for (int i = 1; i < numFrames; i++) {
+		if (page_table[i].usage_data < min_usage) {
+			min_usage = page_table[i].usage_data;
+			min_usage_index = i;
+		}
+	}
+	return min_usage_index;
+}
+
+// Returns the index of the first entry with a usage value of 0
+int findClockVictim() 
+{
+	while(page_table[clock_hand].usage_data != 0) {
+		page_table[clock_hand].usage_data = 0;
+		clock_hand++;
+		if (clock_hand >= numFrames) clock_hand %= numFrames;
+		
+	}
+	return clock_hand;
 }
 
 /* Selects a victim for eviction/discard according to the replacement algorithm,  returns chosen frame_no  */
-page    selectVictim(int page_number, enum repl  mode )
+page selectVictim(int page_number, int event_number, enum repl  mode )
 {
-        page    victim;
-        // to do 
-        victim.pageNo = 0;
-        victim.modified = 0;
-        return (victim) ;
+        page victim;
+		int victim_frame_no = 0;
+		switch (mode) {
+			case lru:
+			case fifo:
+				victim_frame_no = findMinUsage();
+				break;
+			case rng:
+				victim_frame_no = rand() % numFrames;
+				break;
+			case clock:
+				victim_frame_no = findClockVictim();
+				break;
+		}
+		// Copy victim frame data out
+		victim.pageNo = page_table[victim_frame_no].page.pageNo;
+		victim.modified = page_table[victim_frame_no].page.modified;
+
+		// Swap new page in to the victim frame
+		page_table[victim_frame_no].page.pageNo = page_number;
+		page_table[victim_frame_no].page.modified = 0;
+		switch (mode) {
+			case lru:
+			case fifo:
+				page_table[victim_frame_no].usage_data = event_number;
+				break;
+			case clock:
+			case rng:
+				page_table[victim_frame_no].usage_data = 1;
+				break;
+		}
+        return (victim);
 }
 
 		
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   
 	char	*tracename;
@@ -90,7 +187,7 @@ main(int argc, char *argv[])
         if (strcmp(argv[3], "lru\0") == 0) {
             replace = lru;
 		} else if (strcmp(argv[3], "rand\0") == 0) {
-	     replace = random;
+	     replace = rng;
 		} else if (strcmp(argv[3], "clock\0") == 0) {
             replace = clock;		 
 		} else if (strcmp(argv[3], "fifo\0") == 0) {
@@ -126,34 +223,35 @@ main(int argc, char *argv[])
 	while (do_line == 2)
 	{
 		page_number =  address >> pageoffset;
-		frame_no = checkInMemory( page_number) ;    /* ask for physical address */
+		frame_no = checkInMemory(page_number, no_events, replace) ;    /* ask for physical address */
 
 		if ( frame_no == -1 )
 		{
 		  	disk_reads++ ;			/* Page fault, need to load it into memory */
 		  	if (debugmode) {
-		      printf( "Page fault %8d \n", page_number);
+		      printf( "Page fault %8x \n", page_number);
 			}
 		  	if (allocated < numFrames) {  			/* allocate it to an empty frame */
-                frame_no = allocateFrame(page_number);
+                frame_no = allocateFrame(page_number, no_events, replace);
 		     	allocated++;
             } else {
-		      	Pvictim = selectVictim(page_number, replace);   /* returns page number of the victim  */
-		      	frame_no = checkInMemory( page_number);    /* find out the frame the new page is in */
+		      	Pvictim = selectVictim(page_number, no_events, replace);   /* returns page number of the victim  */
+		      	frame_no = checkInMemory(page_number, no_events, replace);    /* find out the frame the new page is in */
 		   		if (Pvictim.modified) {          /* need to know victim page and modified  */
                     disk_writes++;			    
-                    if (debugmode) printf( "Disk write %8d \n", Pvictim.pageNo);
+                    if (debugmode) printf( "Disk write %8x \n", Pvictim.pageNo);
 		      	}
 		   		else {
-                    if (debugmode) printf( "Discard    %8d \n", Pvictim.pageNo);
+                    if (debugmode) printf( "Discard    %8x \n", Pvictim.pageNo);
 				}
 		   	}
 		}
 		if ( rw == 'R') {
-		    if (debugmode) printf( "reading    %8d \n", page_number);
+		    if (debugmode) printf( "reading    %8x \n", page_number);
 		} else if ( rw == 'W'){
-		    // mark page in page table as written - modified  
-		    if (debugmode) printf( "writting   %8d \n", page_number);
+		    // mark page in page table as written - modified
+			page_table[frame_no].page.modified = 1;
+		    if (debugmode) printf( "writting   %8x \n", page_number);
 		} else {
 		      printf( "Badly formatted file. Error on line %d\n", no_events+1); 
 		      exit (-1);
